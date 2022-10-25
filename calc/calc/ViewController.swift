5d51058c48bb72ce7979047c70e9c5097ddc65d6
Emerson M Luz <emerson.maciel@vortigo.com.br>
2022-10-25 16:38:32 -0300

    Adicionado para aparecer números no visor com clicados nos números

--- calc/calc/ViewController.swift
+++ calc/calc/ViewController.swift
@@ -9,11 +9,65 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var numbers: [UIButton]!
+    
+    @IBOutlet weak var lbVIsor: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func pressZero(_ sender: Any) {
+        lbVIsor.text = ("\(String(describing: lbVIsor.text!))" + "0")
+    }
+    
+    
+    @IBAction func pressOne(_ sender: Any) {
+        lbVIsor.text = ("\(String(describing: lbVIsor.text!))" + "1")
+    }
+    
+    
+    @IBAction func pressTwo(_ sender: Any) {
+        lbVIsor.text = ("\(String(describing: lbVIsor.text!))" + "2")
+    }
+    
+    
+    
+    @IBAction func pressTree(_ sender: Any) {
+        lbVIsor.text = ("\(String(describing: lbVIsor.text!))" + "3")
+    }
+    
+    
+    @IBAction func pressFour(_ sender: Any) {
+        lbVIsor.text = ("\(String(describing: lbVIsor.text!))" + "4")
+    }
+    
+    
+    @IBAction func pressFive(_ sender: Any) {
+        lbVIsor.text = ("\(String(describing: lbVIsor.text!))" + "5")
+    }
+    
+    
+    @IBAction func pressSix(_ sender: Any) {
+        lbVIsor.text = ("\(String(describing: lbVIsor.text!))" + "6")
+    }
+    
+    
+    @IBAction func pressSeven(_ sender: Any) {
+        lbVIsor.text = ("\(String(describing: lbVIsor.text!))" + "7")
+    }
+    
+    
+    @IBAction func pressEight(_ sender: Any) {
+        lbVIsor.text = ("\(String(describing: lbVIsor.text!))" + "8")
+    }
+    
+    
+    @IBAction func pressNine(_ sender: Any) {
+        lbVIsor.text = ("\(String(describing: lbVIsor.text!))" + "9")
+    }
+    
 }
 
