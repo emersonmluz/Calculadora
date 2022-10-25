8b9f441345b858f76ee416fbb1e947d7b1d91509
Emerson M Luz <emerson.maciel@vortigo.com.br>
2022-10-25 15:55:07 -0300

    Calculadora iniciada

---
+++ calc/calc/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  calc
+//
+//  Created by Émerson M Luz on 25/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
