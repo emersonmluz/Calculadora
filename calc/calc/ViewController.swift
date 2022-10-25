d09ccb5ad32ade4a4bee6136d06703bc6155e0a2
Emerson M Luz <emerson.maciel@vortigo.com.br>
2022-10-25 17:03:41 -0300

    code em andamento

--- calc/calc/ViewController.swift
+++ calc/calc/ViewController.swift
@@ -10,8 +10,11 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet var numbers: [UIButton]!
-    
     @IBOutlet weak var lbVIsor: UILabel!
+    
+    var operador: String = ""
+    var value1: Int = 0
+    var value2: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -67,6 +70,48 @@ class ViewController: UIViewController {
     
     @IBAction func pressNine(_ sender: Any) {
         lbVIsor.text = ("\(String(describing: lbVIsor.text!))" + "9")
+    }
+    
+    
+    @IBAction func pressAC(_ sender: Any) {
+        lbVIsor.text = "0"
+    }
+    
+    
+    @IBAction func pressPow(_ sender: Any) {
+        print(pow(lbVIsor.text, lbVIsor.text))
+    }
+    
+    
+    @IBAction func divisor(_ sender: Any) {
+        operador = "/"
+        lbVIsor.text = "0"
+    }
+    
+    @IBAction func multiplique(_ sender: Any) {
+        operador = "*"
+        lbVIsor.text = "0"
+    }
+    
+    @IBAction func subtract(_ sender: Any) {
+        operador = "-"
+        lbVIsor.text = "0"
+    }
+    
+    
+    @IBAction func sum(_ sender: Any) {
+        operador = "+"
+        lbVIsor.text = "0"
+    }
+    
+    
+    @IBAction func percentage(_ sender: Any) {
+        operador = "%"
+        lbVIsor.text = "0"
+    }
+    
+    
+    @IBAction func equal(_ sender: Any) {
     }
     
 }
